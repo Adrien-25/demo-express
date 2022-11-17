25bdd763590fa5934b86baf23fa9605a0482c84b
Adrien <adrien.schmidt7@gmail.com>
2022-11-17 09:28:11 +0100

    Début projet express

---
+++ index.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const port = 3000;
+
+const app = express();
+
+app.get("/",(requete, response)=> {
+    response.send("Bonjour le monde");
+});
+
+app.listen(port, () => {
+    console.log(`Le serveur est démarré sur http://localhost:${port}`);
+})
