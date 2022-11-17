293140aae192f5e83ce57bda2e231d47a275cd45
Adrien <adrien.schmidt7@gmail.com>
2022-11-17 09:43:18 +0100

    Ajout des chemins vers la page d'accueil et la page apropos

--- index.js
+++ index.js
@@ -1,12 +1,18 @@
 const express = require("express");
+const path = require("path");
 const port = 3000;
 
 const app = express();
 
+app.use(express.static(path.join(__dirname, './phantom')));
+
 app.get("/",(requete, response)=> {
-    response.send("Bonjour le monde");
+    response.sendFile(path.join(__dirname, './phantom/index.html'));
+});
+app.get("/apropos",(requete, response)=> {
+    response.sendFile(path.join(__dirname, './phantom/generic.html'));
 });
 
 app.listen(port, () => {
-    console.log(`Le serveur est démarré sur http://localhost:${port}`);
+    console.log(`Le serveur est démarré sur http://localhost:${port}`); 
 });
